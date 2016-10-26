2316248bda3354dbf0ab971ef7fd716be77b9ca2
Micha Alt <mail@lean-stack.de>
2016-10-26 22:01:12 +0200

    Bootstrap integrated.

--- brunch-config.js
+++ brunch-config.js
@@ -1,11 +1,32 @@
 
 module.exports = {
   files: {
-    javascripts: {joinTo: 'app.js'},
-    stylesheets: {joinTo: 'app.css'},
+    javascripts: {
+      joinTo: {
+        'js/vendors.js': /^(?!app)/,
+        'js/app.js': /^app/
+      }
+    },
+    stylesheets: {
+      joinTo: {
+        'css/vendors.css': /^(?!app)/,
+        'css/app.css': /^app/
+      }
+    },
     templates: {joinTo: 'app.js'}
   },
   paths: {
-    public: 'docs/'
+    'public': 'docs/'
+  },
+  npm: {
+    styles: {
+      bootstrap: ['dist/css/bootstrap.css']
+    },
+    globals: {
+      '$': 'jquery',
+      'jQuery': 'jquery',
+      'Tether': 'tether'
+    },
+    static: ['node_modules/bootstrap/dist/js/bootstrap.js']
   }
 };
